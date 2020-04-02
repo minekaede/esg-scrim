292c82a117d584af792e97c5d0770d4b937a83ee
minekaede <minek4ede@gmail.com>
2020-04-03 01:09:29 +0900

    Add result.js

---
+++ result.js
@@ -0,0 +1,15 @@
+$(function() {
+    $("#drop-zone").on("drop", function(e) {
+        e.preventDefault();
+        var $result_zone = $("#result-zone");
+        var files = e.originalEvent.dataTransfer.files[0];
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $result_zone.html(e.target.result);
+        }
+    }).on("dragenter", function() {
+        return false;
+    }).on("dragover", function() {
+        return false;
+    });
+});
