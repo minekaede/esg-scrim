f3cd9db08bd3597ff290a01dfefdcdd206418415
minekaede <minek4ede@gmail.com>
2020-04-03 01:31:56 +0900

    Testing FileAPI

--- result.js
+++ result.js
@@ -1,13 +1,14 @@
 $(function() {
     $("#drop-zone").on("drop", function(e) {
-        console.log("File Droped!");
         e.preventDefault();
         var $result_zone = $("#result-zone");
         var files = e.originalEvent.dataTransfer.files[0];
         var reader = new FileReader();
-        reader.onload = function(e) {
+        console.log("Test!");
+        reader.onload = function(event) {
             console.log("File loaded!");
         }
+        reader.readAsText(files, "UTF-8");
     }).on("dragenter", function() {
         return false;
     }).on("dragover", function() {
