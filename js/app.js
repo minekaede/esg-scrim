c4dc6d2dfc371538d3f9131867dde54d17f49c2d
minekaede <minek4ede@gmail.com>
2020-04-03 19:26:12 +0900

    Testing datatable

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ function drawGameTable(data) {
         );
     }
     $("#game").append(tbody);
-    $("#game").DataTable(DATATABLESETTINGS);
+    $("#game").DataTable();
 }
 
 function loadResult(text) {
@@ -44,6 +44,11 @@ function loadResult(text) {
 }
 
 $(function() {
+    $.extend($.fn.dataTable.defaults, {
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Japanese.json"
+        }
+    });
     var csv_url = getParam("url");
     if (csv_url) {
         var xhr = new XMLHttpRequest();
