af2302aa60e3a8393201c3b628769c421f8e30b1
minekaede <minek4ede@gmail.com>
2020-04-11 21:41:16 +0900

    Fix bomb analysis

--- js/app.js
+++ js/app.js
@@ -36,11 +36,16 @@ function drawBombAnalysis() {
     var result = {};
     var point_list = [...new Set(filtered_round.map(r => r.point))];
     point_list.forEach(p => {
-        console.log(p);
-        result[p].offense.win = 0;
-        result[p].offense.lose = 0;
-        result[p].defense.win = 0;
-        result[p].defense.lose = 0;
+        result[p] = {
+            offense: {
+                win: 0,
+                lose: 0
+            },
+            defense: {
+                win: 0,
+                lose:0
+            }
+        };
     });
     filtered_round.forEach(r => {
         result[r.point][r.od][r.wl]++;
