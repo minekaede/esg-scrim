b8545229022e988683ddd3ed5677a688212a288e
minekaede <minek4ede@gmail.com>
2020-04-12 22:47:32 +0900

    Fix bomb table bug

--- js/app.js
+++ js/app.js
@@ -243,6 +243,7 @@ function drawBombAnalysis() {
                 .append($("<th>").text(String("防")))
                 .append($("<th>").text(String(result[p].defense.win)))
                 .append($("<th>").text(String(result[p].defense.lose)))
+                .append($("<th>").text(String(result[p].offense.win + result[p].offense.lose)))
                 .append($("<th>").text(String(result[p].defense.rate)))
             );
         }
